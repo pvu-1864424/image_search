c15494fe00092a0cf57bf2cc89af2d7a8577e97c
phuongvu <pvu@uw.edu>
2020-06-24 11:41:57 +0700

    fixing bugs

--- search.py
+++ search.py
@@ -4,7 +4,7 @@ import argparse
 import cv2
 
 ap = argparse.ArgumentParser()
-ap.add_argument('-i', '--index', required=True,
+ap.add_argument('-i', '--extract_features', required=True,
     help = 'Path to where the computed index will be stored')
 ap.add_argument('-q', '--query', required=True,
     help = 'Path to the query image')
@@ -13,3 +13,16 @@ ap.add_argument('-r', '--result-path', required = True,
 args = vars(ap.parse_args())
 
 cd = ColorDescriptor((8, 12, 3))
+
+query = cv2.imread(args['query'])
+features = cd.describe(query)
+
+searcher = Searcher(args['index'])
+results = searcher.search(features)
+
+cv2.imshow('Query', query)
+
+for (score, resultID) in results:
+    result = cv2.imread(args['result_path'] + '/' + resultID)
+    cv2.imshow('Result', result)
+    cv2.waitKey(0)
